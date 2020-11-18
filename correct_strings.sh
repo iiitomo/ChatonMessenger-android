9e68b7e9f7f1b83cf5d7ae69b278d908b4bd5727
iiitomo <58949423+iiitomo@users.noreply.github.com>
2020-11-18 10:34:02 +0100

    Update correct_strings.sh

--- correct_strings.sh
+++ correct_strings.sh
@@ -13,18 +13,18 @@ fi
 
 mydir="."
 
-# Element -> SchildiChat
+# SchildiChat -> Chaton
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's|Element|SchildiChat|g' '{}' \;
+    sed -i 's|SchildiChat|Chaton|g' '{}' \;
 # Restore Element where it makes sense
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's/SchildiChat \(Web\|iOS\|Desktop\)/Element \1/g' '{}' \;
+    sed -i 's/Chaton \(Web\|iOS\|Desktop\)/SchildiChat \1/g' '{}' \;
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's|SchildiChat Matrix Services|Element Matrix Services|g' '{}' \;
+    sed -i 's|Chaton Matrix Services|SchildiChat Matrix Services|g' '{}' \;
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's|\("use_latest_riot">.*\)SchildiChat\(.*</string>\)|\1Element\2|g' '{}' \;
+    sed -i 's|\("use_latest_riot">.*\)Chaton\(.*</string>\)|\1SchildiChat\2|g' '{}' \;
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's|\("use_other_session_content_description">.*\)SchildiChat\(.*SchildiChat.*</string>\)|\1SchildiChat/Element\2|' '{}' \;
+    sed -i 's|\("use_other_session_content_description">.*\)Chaton\(.*Chaton.*</string>\)|\1Chaton/SchildiChat\2|' '{}' \;
 
 # Requires manual intervention for correct grammar
 sed -i 's|!nnen|wolpertinger|g' "$mydir/vector/src/main/res/values-de/strings.xml"
@@ -34,7 +34,7 @@ sed -i 's|!n|schlumpfwesen|g' "$mydir/vector/src/main/res/values-de/strings.xml"
 rm -rf "$mydir/vector/src/main/play/listings"
 
 git add -A
-git commit -m "Automatic SchildiChat string correction"
+git commit -m "Automatic Chaton string correction"
 
 popd > /dev/null
 
